da82931fcecd4f1ed30e18d42ecb1d11eaf72203
ZamanidisAlexios <sdi1600047@di.uoa.gr>
2019-11-10 17:49:30 +0200

    add vector expand macro

--- header/vector.h
+++ header/vector.h
@@ -19,4 +19,20 @@
         (vector)->data_size = sizeof(*((vector)->data));    \
     } while (0)
 
+#define vector_expand(vector,expand)                                                                                        \
+    void *temp_pointer;                                                                                                     \
+    int new_capacity = (vector)->capacity + (vector)->data_size;                                                            \
+    /* check if there is enought space, if it does return 0 */                                                              \
+    if ((vector)->length+1 < (vector)->capacity)                                                                            \
+        expand = SUCCESS;                                                                                                   \
+    /* otherwise expand fix data structure and return 0 on success or -1 on failure */                                      \
+    else {                                                                                                                  \
+        temp_pointer = realloc((vector)->data,new_capacity*(vector)->data_size);                                            \
+        if ( temp_pointer == NULL )                                                                                         \
+            expand = FAILURE;                                                                                               \
+        (vector)->data = temp_pointer;                                                                                      \
+        (vector)->capacity = new_capacity;                                                                                  \
+        expand = SUCCESS;                                                                                                   \
+    }
+
 #endif
