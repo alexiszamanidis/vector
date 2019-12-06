e90c68814f1b90d15b20ae7f026728e6dae530b9
ZamanidisAlexios <sdi1600047@di.uoa.gr>
2019-12-06 23:25:33 +0200

    update vector header

--- header/vector.h
+++ header/vector.h
@@ -19,44 +19,48 @@
         (vector)->data_size = sizeof(*((vector)->data));    \
     } while (0)
 
-#define vector_expand(vector,expand)                                                                                        \
-    void *temp_pointer;                                                                                                     \
-    int new_capacity = (vector)->capacity + (vector)->data_size;                                                            \
-    /* check if there is enought space, if it does return 0 */                                                              \
-    if ((vector)->length+1 < (vector)->capacity)                                                                            \
-        expand = SUCCESS;                                                                                                   \
-    /* otherwise expand fix data structure and return 0 on success or -1 on failure */                                      \
-    else {                                                                                                                  \
-        temp_pointer = realloc((vector)->data,new_capacity*(vector)->data_size);                                            \
-        if ( temp_pointer == NULL )                                                                                         \
-            expand = FAILURE;                                                                                               \
-        (vector)->data = temp_pointer;                                                                                      \
-        (vector)->capacity = new_capacity;                                                                                  \
-        expand = SUCCESS;                                                                                                   \
-    }
+#define vector_expand(vector,expand)                                                                                            \
+    do {                                                                                                                        \
+        void *temp_pointer;                                                                                                     \
+        int new_capacity = (vector)->capacity + (vector)->data_size;                                                            \
+        /* check if there is enought space, if it does return 0 */                                                              \
+        if ((vector)->length+1 < (vector)->capacity)                                                                            \
+            expand = SUCCESS;                                                                                                   \
+        /* otherwise expand fix data structure and return 0 on success or -1 on failure */                                      \
+        else {                                                                                                                  \
+            temp_pointer = realloc((vector)->data,new_capacity*(vector)->data_size);                                            \
+            if ( temp_pointer == NULL )                                                                                         \
+                expand = FAILURE;                                                                                               \
+            else {                                                                                                              \
+                (vector)->data = temp_pointer;                                                                                  \
+                (vector)->capacity = new_capacity;                                                                              \
+                expand = SUCCESS;                                                                                               \
+            }                                                                                                                   \
+        }                                                                                                                       \
+    } while (0)
 
-#define vector_push_at_nth_position(vector, value, position)                                                                \
-  do {                                                                                                                      \
-    char *source, *dest;                                                                                                    \
-    int expand, size, decrement_position = position-1;                                                                      \
-    /* if the position is valid */                                                                                          \
-    if( (decrement_position >= 0) && (decrement_position <= ((vector)->length)) ) {                                         \
-        vector_expand(vector,expand);                                                                                       \
-        /* if the expand was successful, do the push */                                                                     \
-        if( expand == SUCCESS ) {                                                                                           \
-            dest = (char *)(vector)->data + (decrement_position + 1) * (vector)->data_size;                                 \
-            source = (char *)(vector)->data + decrement_position * (vector)->data_size;                                     \
-            size = ((vector)->length - decrement_position) * (vector)->data_size;                                           \
-            memmove(dest,source,size);                                                                                      \
-            (vector)->data[decrement_position] = value;                                                                     \
-            (vector)->length++;                                                                                             \
-        }                                                                                                                   \
-        else                                                                                                                \
-            printf("Could not expand. Push at < %d > position failed\n",position);                                          \
-    }                                                                                                                       \
-    else                                                                                                                    \
-        printf("Wrong position < %d >.\n",position);                                                                        \
-  } while (0)
+#define vector_push_at_nth_position(vector, value, position)                                                                    \
+    do {                                                                                                                        \
+        char *source, *dest;                                                                                                    \
+        int expand, size, decrement_position = position-1;                                                                      \
+        /* if the position is valid */                                                                                          \
+        if( (decrement_position >= 0) && (decrement_position <= ((vector)->length)) ) {                                         \
+            vector_expand(vector,expand);                                                                                       \
+            /* if the expand was successful, do the push */                                                                     \
+            if( expand == SUCCESS ) {                                                                                           \
+                dest = (char *)(vector)->data + (decrement_position + 1) * (vector)->data_size;                                 \
+                source = (char *)(vector)->data + decrement_position * (vector)->data_size;                                     \
+                size = ((vector)->length - decrement_position) * (vector)->data_size;                                           \
+                memmove(dest,source,size);                                                                                      \
+                (vector)->data[decrement_position] = value;                                                                     \
+                (vector)->length++;                                                                                             \
+            }                                                                                                                   \
+            else                                                                                                                \
+                printf("Could not expand. Push at < %d > position failed\n",position);                                          \
+        }                                                                                                                       \
+        else                                                                                                                    \
+            printf("Wrong position < %d >.\n",position);                                                                        \
+    } while (0)
 
 #define vector_push_front(vector, value)                                        \
     do {                                                                        \
